f035a2693c038cb56df5edc8aa70189fa7097f22
Alexandre <alexandretavares@outlook.com>
2025-06-04 20:29:51 -0300

    Solve Content.test.ts problem

--- src/components/content/Content.test.ts
+++ src/components/content/Content.test.ts
@@ -1,10 +1,41 @@
-
 import { Content } from "./Content";
+import { DependencyContainer } from "@/core/DependencyContainer";
+
+class MockShortcutListeners {
+  bind = jest.fn();
+  unbind = jest.fn();
+  clear = jest.fn();
+}
+
+class MockTableListeners {
+  bind = jest.fn();
+  unbind = jest.fn();
+  clear = jest.fn();
+}
+
+class MockQuickMenu {
+  open = jest.fn();
+  close = jest.fn();
+  toggle = jest.fn();
+  register = jest.fn();
+}
+
+class MockTableContextFloatingToolbar {
+  show = jest.fn();
+  hide = jest.fn();
+  update = jest.fn();
+}
 
 describe("Content", () => {
-    test("Create element", () => {
-        const content = Content.getInstance();
+  beforeEach(() => {
+    DependencyContainer.Instance.register("IShortcutListeners", () => new MockShortcutListeners());
+    DependencyContainer.Instance.register("ITableListeners", () => new MockTableListeners());
+    DependencyContainer.Instance.register("IQuickMenu", () => new MockQuickMenu());
+    DependencyContainer.Instance.register("ITableContextFloatingToolbar", () => new MockTableContextFloatingToolbar());
+  });
 
-        expect(content).toBeInstanceOf(Content);
-    });
+  test("Create element", () => {
+    const content = Content.getInstance();
+    expect(content).toBeInstanceOf(Content);
+  });
 });
