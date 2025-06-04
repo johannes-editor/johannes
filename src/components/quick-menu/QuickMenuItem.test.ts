37049cfe7a6940449dcf2fa4b82859e2f7cea547
Alexandre <alexandretavares@outlook.com>
2025-06-04 20:25:58 -0300

    Solve QuickMenuItem.test.ts test error

--- src/components/quick-menu/QuickMenuItem.test.ts
+++ src/components/quick-menu/QuickMenuItem.test.ts
@@ -1,16 +1,31 @@
 import { QuickMenu } from "./QuickMenu";
-import { QuickMenuItem } from "./QuickMenuItem"
+import { QuickMenuItem } from "./QuickMenuItem";
 import { QuickMenuSection } from "./QuickMenuSection";
+import { DependencyContainer } from "@/core/DependencyContainer";
+
+class MockFocusStack {
+  push = jest.fn();
+  pop = jest.fn();
+  peek = jest.fn();
+  clear = jest.fn();
+}
 
 describe("QuickMenuItem", () => {
+  beforeEach(() => {
+    DependencyContainer.Instance.register("IFocusStack", () => new MockFocusStack());
+  });
 
-    test("Create QuickMenuItem with success", () => {
+  test("Create QuickMenuItem with success", () => {
+    const quickMenu = QuickMenu.getInstance();
 
-        const quickMenu = QuickMenu.getInstance();
-        const quickMenuSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: "", classList: "a" });
-
-        const quickMenuItem = new QuickMenuItem(quickMenuSection, "a", "b", "c", "d", "e");
-
-        expect(quickMenuItem).toBeInstanceOf(QuickMenuItem);
+    const quickMenuSection = new QuickMenuSection({
+      quickMenuInstance: quickMenu,
+      title: "",
+      classList: "a",
     });
+
+    const quickMenuItem = new QuickMenuItem(quickMenuSection, "a", "b", "c", "d", "e");
+
+    expect(quickMenuItem).toBeInstanceOf(QuickMenuItem);
+  });
 });
