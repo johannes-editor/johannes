e2d0c00c62ec68b84bc8484416217eb87b6c4cb5
Alexandre <alexandretavares@outlook.com>
2025-06-04 20:27:04 -0300

    Solve QuickMenuSection.test error

--- src/components/quick-menu/QuickMenuSection.test.ts
+++ src/components/quick-menu/QuickMenuSection.test.ts
@@ -1,14 +1,28 @@
 import { QuickMenu } from "./QuickMenu";
-import { QuickMenuSection } from "./QuickMenuSection"
+import { QuickMenuSection } from "./QuickMenuSection";
+import { DependencyContainer } from "@/core/DependencyContainer";
+
+class MockFocusStack {
+  push = jest.fn();
+  pop = jest.fn();
+  peek = jest.fn();
+  clear = jest.fn();
+}
 
 describe("QuickMenuSection", () => {
+  beforeEach(() => {
+    DependencyContainer.Instance.register("IFocusStack", () => new MockFocusStack());
+  });
 
-    test("Create instance with success", () => {
+  test("Create instance with success", () => {
+    const quickMenu = QuickMenu.getInstance();
 
-        const quickMenu = QuickMenu.getInstance();
-
-        const quickMenuSection = new QuickMenuSection({ quickMenuInstance: quickMenu, title: "a", classList: "a" });
-
-        expect(quickMenuSection).toBeInstanceOf(QuickMenuSection);
+    const quickMenuSection = new QuickMenuSection({
+      quickMenuInstance: quickMenu,
+      title: "a",
+      classList: "a",
     });
+
+    expect(quickMenuSection).toBeInstanceOf(QuickMenuSection);
+  });
 });
