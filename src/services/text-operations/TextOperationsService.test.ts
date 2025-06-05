c0044c37ce3fba07bcbb664e898f0d0a52fd60c7
Alexandre <alexandretavares@outlook.com>
2025-06-04 21:07:04 -0300

    Solve TextOperationService.test error

--- src/services/text-operations/TextOperationsService.test.ts
+++ src/services/text-operations/TextOperationsService.test.ts
@@ -1,6 +1,20 @@
 import { TextOperationsService } from "./TextOperationsService"
+import { DependencyContainer } from "@/core/DependencyContainer";
+
+class MockMemento {
+    get = jest.fn();
+    set = jest.fn();
+    undo = jest.fn();
+    redo = jest.fn();
+    saveState = jest.fn();
+}
 
 describe("textOperationService", () => {
+
+    beforeEach(() => {
+        DependencyContainer.Instance.register("IMemento", () => new MockMemento());
+    });
+
     test("getInstance with success", () => {
 
         const textOperationService = TextOperationsService.getInstance();
