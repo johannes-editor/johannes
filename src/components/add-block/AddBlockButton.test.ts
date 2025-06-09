fbc6acd7b3959fc3f3c7b0cd8ecfa6722a0b0d45
Alexandre Tavares <alexandretavares@outlook.com>
2025-06-09 20:58:28 -0300

    Add test for adding block when only empty paragraph

--- src/components/add-block/AddBlockButton.test.ts
+++ src/components/add-block/AddBlockButton.test.ts
@@ -1,97 +1,43 @@
-import { UIBuilder } from "../../builders/UIBuilder";
 import { Editor } from "../editor/Editor";
-
+import { EditorBuilder } from "@/builders/EditorBuilder";
+import { DependencyContainer } from "@/core/DependencyContainer";
+import { registerEditorDependenciesForTests } from "../../../tests/helpers/registerEditorDependencies";
+import { ElementFactoryService } from "@/services/element-factory/ElementFactoryService";
+import { BlockOperationsService } from "@/services/block-operations/BlockOperationsService";
+import { FocusStack } from "@/core/FocusStack";
+import { Memento } from "@/core/Memento";
 
 describe("AddBlockButton", () => {
-    test("dummy", () => {
-        expect(true).toBe(true);
+    beforeEach(() => {
+        document.body.innerHTML = '<div id="johannesEditor"></div>';
+        (window as any).editorConfig = { enableAddBlock: true };
+
+        registerEditorDependenciesForTests();
+        DependencyContainer.Instance.register('IElementFactoryService', () => ElementFactoryService.getInstance());
+        DependencyContainer.Instance.register('IFocusStack', () => FocusStack.getInstance());
+        DependencyContainer.Instance.register('IMemento', () => Memento.getInstance());
+        DependencyContainer.Instance.register('IBlockOperationsService', () => BlockOperationsService.getInstance());
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        document.body.innerHTML = '';
+    });
+
+    test("adds a new paragraph when the only existing block is empty", (done) => {
+        const editor = EditorBuilder.build();
+        expect(editor).toBeInstanceOf(Editor);
+
+        const content = document.querySelector('#johannesEditor .content') as HTMLElement;
+        const addBlockButton = document.querySelector('.add-block') as HTMLButtonElement;
+
+        expect(content.querySelectorAll('.block').length).toBe(1);
+
+        addBlockButton.click();
+
+        setTimeout(() => {
+            expect(content.querySelectorAll('.block').length).toBe(2);
+            done();
+        }, 0);
     });
 });
-
-// describe("AddBlockButton", () => {
-
-//     beforeEach(() => {
-//         document.body.innerHTML = `
-//         <div id="johannesEditor"></div>
-//     `;
-
-//         const editor = UIBuilder.build().start();
-//         expect(editor).toBeInstanceOf(Editor);
-//     });
-
-//     test("Add functionality", () => {
-
-//         let paragraphs = document.querySelectorAll("#johannesEditor .content p");
-//         expect(paragraphs.length).toBe(1);
-
-//         const addBlockButton = document.querySelector(".add-block") as HTMLButtonElement;
-
-//         addBlockButton.click();
-
-//         paragraphs = document.querySelectorAll("#johannesEditor .content p");
-//         expect(paragraphs.length).toBe(2);
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-// // import { UIBuilder } from "../../builders/UIBuilder";
-// // import { Editor } from "../editor/Editor";
-
-// // describe("AddBlockButton", () => {
-
-// //     beforeEach(() => {
-// //         // Setup the editor in the document body
-// //         document.body.innerHTML = `
-// //         <div id="johannesEditor"></div>
-// //         <script>
-// //             const editorConfig = {
-// //                 enableFloatingToolbar: true,
-// //                 enableQuickMenu: true,
-// //                 enableAddBlock: true,
-// //                 includeHeader: true,
-// //                 includeFirstParagraph: true,
-// //                 enableTitle: true,
-// //                 title: undefined
-// //             };
-// //             window.editorConfig = editorConfig;
-// //         </script>
-// //     `;
-
-// //         const editorConfig = {
-// //             enableFloatingToolbar: true,
-// //             enableQuickMenu: true,
-// //             enableAddBlock: true,
-// //             includeHeader: true,
-// //             includeFirstParagraph: true,
-// //             enableTitle: true,
-// //             title: undefined
-// //         };
-// //         window.editorConfig = editorConfig;
-
-// //         const editor = UIBuilder.build().start();
-// //         expect(editor).toBeInstanceOf(Editor);
-// //     });
-
-// //     test("Add functionality", () => {
-
-// //         let paragraphs = document.querySelectorAll("#johannesEditor .content p");
-// //         expect(paragraphs.length).toBe(1);
-
-// //         const addBlockButton = document.querySelector(".add-block") as HTMLButtonElement;
-
-// //         addBlockButton.click();
-
-// //         paragraphs = document.querySelectorAll("#johannesEditor .content p");
-// //         expect(paragraphs.length).toBe(2);
-// //     });
-// // });
